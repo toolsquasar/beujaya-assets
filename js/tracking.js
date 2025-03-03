e933bd23c7a7ed229c48b1f0efeff5e462a23b55
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2025-03-03 22:42:48 +0700

    Create tracking.js

---
+++ js/tracking.js
@@ -0,0 +1,16 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4908955&101"/>');
+
+///// Statcounter /////
+var sc_project=13060317; 
+var sc_invisible=1; 
+var sc_security="728d3e8a";
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13060317/0/728d3e8a/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
